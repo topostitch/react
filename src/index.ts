951d394200b7f6805f0fbf69d850bb61b5934635
lavosbit <jacob@lavosbit.com>
2026-07-05 01:51:21 -0400

    fix: export renderer registry

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./components/ObjectViewer";
 export * from "./renderers/react-three-fiber/ReactThreeFiberRenderer";
 export * from "./renderers";
+export { renderers } from "./renderers";
